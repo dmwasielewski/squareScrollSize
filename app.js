bd3c35ba4995cd415765f6e7e19205d459ad6030
Damian Wasielewski <wasielewski.damian@gmail.com>
2022-09-05 13:53:11 +0100

    app js

--- app.js
+++ app.js
@@ -2,5 +2,28 @@
 
 const square = document.createElement('div');
 document.body.appendChild(square);
-square.style.width = '100px';
-square.style.height = '100px';
+
+let grow = true;
+let size = 100;
+square.style.width = size + 'px';
+square.style.height = size + 'px';
+
+square.style.backgroundColor = 'aquamarine';
+
+window.addEventListener('scroll', function () {
+    if (grow) {
+        size += 5;
+        square.style.width = size + 'px';
+        square.style.height = size + 'px';
+    } else {
+        size -= 5;
+        square.style.width = size + 'px';
+        square.style.height = size + 'px';
+    }
+
+    if (size >= this.window.innerWidth / 2) {
+        grow = !grow;
+    } else if (size == 0) {
+        grow = !grow;
+    }
+});
